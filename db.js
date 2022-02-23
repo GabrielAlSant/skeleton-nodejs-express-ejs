cc3fc5d9d515fe49ae468c8285dd5a7b235c58ff
Gabriel Alves dos Santos <86124609+GabrielAlSant@users.noreply.github.com>
2022-02-23 20:56:35 +0000

    Adicionada conexão com o banco de dados ElephantSQL.

---
+++ db.js
@@ -0,0 +1,14 @@
+let connect = function(){
+if(global.connection){
+  return global.conncetion.connect();
+}
+  const { Pool } = require("pg");
+  const pool = new Pool({
+    connectionString:"postgres://zhmyoxyk:zWO6MyI2KzlfmKf90QwD1UobnhXtTQGt@kesavan.db.elephantsql.com/zhmyoxyk"
+  });
+
+  global.connection = pool 
+  return pool.connect();
+}
+
+module.exports = { connect }
