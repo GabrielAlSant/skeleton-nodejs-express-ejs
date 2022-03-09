cb281de366a2ad44f1d87ffa1ad229614c98a253
Gabriel Alves dos Santos <86124609+GabrielAlSant@users.noreply.github.com>
2022-03-09 21:11:34 +0000

    Adicionei o inserir.

--- models/autor.js
+++ models/autor.js
@@ -2,8 +2,10 @@ const db = require("../db")
 
 class Autor{
   static  async inserir(){
-      const connect = await db.connect();
+      const connect = await db.connect(data);
     const sql = "insert into autores(nome, sobrenome, datanascimento) values($1, $2, $3)";
+    const values = [data.nome, data.sobrenome, data.datanascimento]
+    return await connect.query(sql,values)
   }
   static async selecionar(){
     const connect = await db.connect();
@@ -12,6 +14,7 @@ class Autor{
   static async atualizar(){
       const connect = await db.connect();
     return await connect.query("Select * from autores")
+    
   }
   static async deletar(){
       const connect = await db.connect();
